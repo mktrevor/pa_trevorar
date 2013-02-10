2e193bf76bbce318e8743a9ec11425e67f511b4f
Trevor Reed <trevorar@usc.edu>
2013-02-10 01:32:18 -0800

    Successful compile

--- sn.cpp
+++ sn.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include "user.h"
 #include "mylist.h"
 #include "gmlreader.h"
@@ -14,19 +15,29 @@ int main(int argc, char *argv[])
     cerr << "Please provide the input GML file, command file, and output file" << endl;
     return 1;
   }
+  
+  MyList<User*> userList;
 
 	vector<string> nodes;
 	vector<string> edges;
 	
 	GMLReader::read(argv[1], nodes, edges);
 	
+	string s;
+	stringstream ss;
+	string infoType;
+	
 	for(int i = 0; i < nodes.size(); i++) {
+		s = nodes[i];
+	}
+	
+	/*for(int i = 0; i < nodes.size(); i++) {
 		cout << nodes[i] << endl;
 	}
 	
 	for(int i = 0; i < edges.size(); i++) {
 		cout << edges[i] << endl;
-	}
+	}*/
 
   return 0;
 }
