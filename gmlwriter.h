002f164498423a1032de066b9bd6e28fb5e2f71c
Trevor Reed <trevorar@usc.edu>
2013-02-10 03:53:30 -0800

    Started writing gmlwriter .h and .cpp files

--- gmlwriter.h
+++ gmlwriter.h
@@ -1,10 +1,22 @@
 #ifndef GML_WRITER_H
 #define GML_WRITER_H
+#include <string>
+#include <iostream>
 #include "user.h"
+#include "mylist.h"
 
+using namespace std;
 
-
-
-
+class GMLWriter() {
+	private:
+		MyList<User*> userList_;
+	
+	public:
+		User();
+		User(MyList<User*> userList);
+		~User();
+		
+		write(MyList<User*>);
+}
 
 #endif
